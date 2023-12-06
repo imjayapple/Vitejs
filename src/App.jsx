dd96d63ce33afebfdf8fc8e2f27081fab9587294
imjayapple <imjayapple@gmail.com>
2023-12-05 21:04:52 -0800

    Remove these placeholder variables, clean slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 const App = () => {
   
-  let x = "JayApple";
-  let array = ["user1", "user2", "user3", "user4", "user5"];
+
   return (
     <div>
-      {x}
-      {array.map((user)=>{return <h2>{user}</h2>})}
+
     </div>
   )
 }
