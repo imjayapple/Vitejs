6adc5d0266e0019567a59ef38b43ebd8d7c65d6f
imjayapple <imjayapple@gmail.com>
2023-12-11 19:45:33 -0800

    Line through effect applied to the toggling action, template literal on todoitems.jsx

--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -3,11 +3,29 @@ import tick from './Assets/tick.png'
 import box from './Assets/box.png'
 import cross from './Assets/cross.png'
 
+const TodoItems = ({no,display,text,setTodos}) => {
 
-const TodoItems = ({no,display,text}) => {
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for(let i = 0;i < data.length;i++)
+    {
+      if (data[i].no===no) {
+        if (data[i].display==="") {
+          data[i].display = "line-through";
+        }
+        else
+        {
+          data[i].display = "";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+  }
+
   return (
     <div className='todoitems'>
-      <div className="todoitems-container">
+      <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
         {display===""?<img src={box} alt=""/>:<img src={tick} alt=""/>}
         <div className="todoitems-text">{text}</div>
       </div>
