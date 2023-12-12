5295fb3a6d09a665eff3b8deedda9b2fe17b962a
imjayapple <imjayapple@gmail.com>
2023-12-11 17:16:53 -0800

    Import the assets: tick, box, and cross

--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,9 +1,18 @@
 import './CSS/TodoItems'
+import tick from './Assets/tick.png'
+import box from './Assets/box.png'
+import cross from './Assets/cross.png'
+
 
 const TodoItems = () => {
   return (
-    <div>
-      
+    <div className='todoitems'>
+      <div className="todoitems-container">
+        <img src={box} alt=""/>
+        <img src={tick} alt=""/>
+        <div className="todoitems-text"></div>
+      </div>
+      <img src={cross} alt=""/>
     </div>
   )
 }
