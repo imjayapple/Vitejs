08e126a377c10279948bcd0d75fb00431145e696
imjayapple <imjayapple@gmail.com>
2023-12-11 17:03:52 -0800

    Added a function for 'Add' button onClick, capture the input and log to console

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react'
 import './CSS/Todo.css'
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
+let count = 0;
 const Todo = () => {
+
+    const [todos,setTodos] = useState([]);
+    const inputRef = useRef(null);
+
+    const add = () => {
+        setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}]);
+        inputRef.current.value = "";
+    }
+
+    useEffect(()=>{
+        console.log(todos);
+    },[todos])
+
   return (
     <div className='todo'>
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-            <input type="text" placeholder='Add Your Task' className='todo-input'/>
-            <div className="todo-add-btn">ADD</div>
+            <input ref={inputRef} type="text" placeholder='Add Your Task' className='todo-input'/>
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
         <div className='todo-list'>
-            
+
         </div>  
     </div>
   )
